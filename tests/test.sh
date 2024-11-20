1874c1dcf707720bca3c5125682e176b768862ef
Jax Young <jaxvanyang@gmail.com>
2024-11-20 11:22:08 +0800

    test: refactor test

--- tests/test.sh
+++ tests/test.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
-# use rere.py, see https://github.com/tsoding/rere.py
-rm -rf a b && mkdir -p a b
+#
+# Simple sanity check
+
+set -e
+
+export PYTHONPATH="$PWD/src"
+
+mkdir -p test && cd test
+mkdir -p a b && rm -rf a/* b/*
 touch a/0.log a/1.log a/1.txt a/2.txt
-pdm run batchlink -S a -d b --dry-run '*' '{path}'
-pdm run batchlink -S a -d b -c --dry-run '*' '{path}'
-pdm run batchlink -S a -d b -R --dry-run '*' '{path}'
-pdm run batchlink -S a -d b -sR --dry-run '*' '{path}'
-pdm run batchlink -S a -d b --dry-run '*.log' '{num}.log'
-pdm run batchlink -S a -d b -c --dry-run '*.log' '{num}.log'
-pdm run batchlink -S a -d b -R --dry-run '*.log' '{num}.log'
-pdm run batchlink -S a -d b -sR --dry-run '*.log' '{num}.log'
-pdm run batchlink -S a -d b -f '*' '{path}'
-pdm run batchlink -S a -d b -c -f '*' '{path}'
-pdm run batchlink -S a -d b -R -f '*' '{path}'
-pdm run batchlink -S a -d b -sR -f '*' '{path}'
-pdm run batchlink -S a -d b -f '*.log' '{num}.log'
-pdm run batchlink -S a -d b -c -f '*.log' '{num}.log'
-pdm run batchlink -S a -d b -R -f '*.log' '{num}.log'
-pdm run batchlink -S a -d b -sR -f '*.log' '{num}.log'
-pdm run batchlink -S a -d b -r --dry-run '*' '{path}'
-pdm run batchlink -S a -d b -r --dry-run '*.log' '{num}.log'
-pdm run batchlink -S a -d b -r -f '*.log' '{num}.log'
-pdm run batchlink -S a -d b -r -f '*' '{path}'
+
+# Dry run all mode combinations
+python3 -m batchlink -S a -d b --dry-run '*' '{path}'
+python3 -m batchlink -S a -d b -c --dry-run '*' '{path}'
+python3 -m batchlink -S a -d b -R --dry-run '*' '{path}'
+python3 -m batchlink -S a -d b -sR --dry-run '*' '{path}'
+python3 -m batchlink -S a -d b -r --dry-run '*' '{path}'
+
+# Same as above, but with different input & output
+python3 -m batchlink -S a -d b --dry-run '*.log' '{num}.log'
+python3 -m batchlink -S a -d b -c --dry-run '*.log' '{num}.log'
+python3 -m batchlink -S a -d b -R --dry-run '*.log' '{num}.log'
+python3 -m batchlink -S a -d b -sR --dry-run '*.log' '{num}.log'
+python3 -m batchlink -S a -d b -r --dry-run '*.log' '{num}.log'
+
+# Real run all mode combinations
+python3 -m batchlink -S a -d b -f '*' '{path}'
+python3 -m batchlink -S a -d b -c -f '*' '{path}'
+python3 -m batchlink -S a -d b -R -f '*' '{path}'
+python3 -m batchlink -S a -d b -sR -f '*' '{path}'
+python3 -m batchlink -S a -d b -r -f '*' '{path}'
+
+# Same as above, but with different input & output
+python3 -m batchlink -S a -d b -f '*.log' '{num}.log'
+python3 -m batchlink -S a -d b -c -f '*.log' '{num}.log'
+python3 -m batchlink -S a -d b -R -f '*.log' '{num}.log'
+python3 -m batchlink -S a -d b -sR -f '*.log' '{num}.log'
+python3 -m batchlink -S a -d b -r -f '*.log' '{num}.log'
